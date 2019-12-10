efdd70efe641071cf58c280badbc20dca2d2e2af
zhubiaobiao <hbkzhu@163.com>
2019-12-10 22:25:54 +0800

    2019.12.10 commit

--- redis-java/src/com/holmeszhu/data_type/ListDataType.java
+++ redis-java/src/com/holmeszhu/data_type/ListDataType.java
@@ -5,10 +5,21 @@ import java.util.List;
 
 public class ListDataType extends CommonDataType {
 
+    public boolean listDataType(String key) {
+        return redisMap.get(key) instanceof List;
+    }
 
-    public long lPush(String key, String value) {
+    /**
+     * @param key
+     * @param value
+     * @return
+     */
+    public int lPush(String key, String value) {
         if (redisMap.containsKey(key)) {
-            LinkedList<String> list = lRange(key);
+            if (!listDataType(key)) {
+                return -1;
+            }
+            LinkedList<String> list = lRange(key, 0, -1);
             list.addFirst(value);
             redisMap.put(key, list);
             return list.size();
@@ -20,10 +31,16 @@ public class ListDataType extends CommonDataType {
         }
     }
 
+//    public int lPushX(String key, String value) {
+//        if (redisMap.containsKey(key)) {
+//
+//        }
+//    }
+
 
     public long rPush(String key, String value) {
         if (redisMap.containsKey(key)) {
-            LinkedList<String> list = lRange(key);
+            LinkedList<String> list = lRange(key, 0, -1);
             list.add(value);
             redisMap.put(key, list);
             return list.size();
@@ -38,7 +55,7 @@ public class ListDataType extends CommonDataType {
 
     public long lLen(String key) {
         if (redisMap.containsKey(key)) {
-            LinkedList<String> linkedList = lRange(key);
+            LinkedList<String> linkedList = lRange(key, 0, -1);
             return linkedList.size();
         } else {
             return 0;
@@ -49,7 +66,7 @@ public class ListDataType extends CommonDataType {
     public String lPop(String key, String value) {
 
         if (redisMap.containsKey(key)) {
-            LinkedList<String> list = lRange(key);
+            LinkedList<String> list = lRange(key, 0, -1);
             list.removeFirst();
         }
         return null;
@@ -60,7 +77,7 @@ public class ListDataType extends CommonDataType {
     public String rPop(String key, String value) {
 
         if (redisMap.containsKey(key)) {
-            LinkedList<String> list = lRange(key);
+            LinkedList<String> list = lRange(key, 0, -1);
             list.removeLast();
         }
         return null;
@@ -68,10 +85,37 @@ public class ListDataType extends CommonDataType {
     }
 
 
-    public LinkedList<String> lRange(String key) {
-        return (LinkedList<String>) redisMap.get(key);
-    }
+    public LinkedList<String> lRange(String key, int start, int stop) {
 
+        LinkedList<String> result = new LinkedList<>();
+        LinkedList<String> list = (LinkedList<String>) redisMap.get(key);
+
+        if (start < 0) {
+            start = list.size() + start;
+        }
+
+        if (stop < 0) {
+            stop = list.size() + stop;
+        }
+
+        if (start > stop) {
+            return result;
+        }
+
+        if (start > list.size() - 1) {
+            return result;
+        }
+        if (stop > list.size() - 1) {
+            stop = list.size() - 1;
+        }
+
+        for (int i = start; i <= stop; i++) {
+            result.add(list.get(i));
+
+        }
+        return result;
+
+    }
 
 
 }
